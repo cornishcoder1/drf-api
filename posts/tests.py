4fbfde14b5ed5c20383d6742896f877d5ac9f4a3
cornishcoder1 <leahfisher07@hotmail.co.uk>
2022-07-24 09:16:22 +0000

    Correct url error in posts tests

--- posts/tests.py
+++ posts/tests.py
@@ -40,22 +40,22 @@ class PostDetailViewTests(APITestCase):
         )
 
     def test_can_retrieve_post_using_valid_id(self):
-        response = self.client.get('/posts/1/')
+        response = self.client.get('/posts/1')
         self.assertEqual(response.data['title'], 'a title')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_cant_retrieve_post_using_invalid_id(self):
-        response = self.client.get('/posts/999/')
+        response = self.client.get('/posts/999')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_user_can_update_own_post(self):
         self.client.login(username='adam', password='pass')
-        response = self.client.put('/posts/1/', {'title': 'a new title'})
+        response = self.client.put('/posts/1', {'title': 'a new title'})
         post = Post.objects.filter(pk=1).first()
         self.assertEqual(post.title, 'a new title')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_user_cant_update_another_users_post(self):
         self.client.login(username='adam', password='pass')
-        response = self.client.put('/posts/2/', {'title': 'a new title'})
+        response = self.client.put('/posts/2', {'title': 'a new title'})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
