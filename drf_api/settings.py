dbdda42b3d5df3a8ff9becc221aaf6af875108e1
cornishcoder1 <leahfisher07@hotmail.co.uk>
2022-08-09 09:22:20 +0000

    Test push

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -80,7 +80,7 @@ if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN'),
         os.environ.get('CLIENT_ORIGIN_DEV')
-   ]
+    ]
 
 else:
     CORS_ALLOWED_ORIGIN_REGEXES = [
